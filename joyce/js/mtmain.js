db65181ae9de6c0dd069d4ebdf1b75341b55dc81
JoyceLiu <57802310+joyceyuqiliu@users.noreply.github.com>
2020-03-27 12:44:57 -0400

    organized the files

--- joyce/js/mtmain.js
+++ joyce/js/mtmain.js
@@ -1,16 +1,10 @@
-var dataset
+var data;
 var stringFilter = "";
 var selectValue = 'All';
 
 /* =======
 map
 ==== */
-var mapOpt = {
-  center: [0,0],
-  zoom: 2
-}; /*what does this do*/
-var map = L.map('map', mapOpts);
-
 //Another options object
 var tileOpts = {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -20,3 +14,16 @@ var tileOpts = {
   ext: 'png'
 };
 var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', tileOpts).addTo(map);
+var myLayer = L.geoJSON().addTo(map);
+myLayer.addData(geojsonFeature);
+// grab geojson data
+var getData = $.ajax('https://github.com/joyceyuqiliu/midterm/raw/master/midterm_dataset/Waste_Mexport3.geojson')
+
+//
+/* =====================
+  Parse and store data for later use
+===================== */
+
+/* =====================
+  Application execution
+===================== */
