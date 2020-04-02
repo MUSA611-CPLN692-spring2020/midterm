2690ecc7f53f8d59babd3062ae8da67a559c9873
JoyceLiu <57802310+joyceyuqiliu@users.noreply.github.com>
2020-04-02 17:54:20 -0400

    update

---
+++ joyce/js/L.topojson.js
@@ -0,0 +1,14 @@
+L.TopoJSON = L.GeoJSON.extend({
+  addData: function(jsonData) {
+    if (jsonData.type === 'Topology') {
+      for (key in jsonData.objects) {
+        geojson = topojson.feature(jsonData, jsonData.objects[key]);
+        L.GeoJSON.prototype.addData.call(this, geojson);
+      }
+    }
+    else {
+      L.GeoJSON.prototype.addData.call(this, jsonData);
+    }
+  }
+});
+// Copyright (c) 2013 Ryan Clark
--- joyce/js/mtmain.js
+++ joyce/js/mtmain.js
@@ -2,27 +2,33 @@ var data;  // for holding data
 var stringFilter = "";
 var selectValue = 'All';
 /* =======
-map
+view topojson map
 ==== */
-var map = L.map('map', {
-  center: [0, 0],
-  zoom: 2
-});
+var map = L.map('worldmap').setView([44,-31], 3);
+var topolayer =
+
+data = $.getJSON('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json')
+.done(addTopoData);
+
+function addTopoData(topoData) {
+  topoLayer.addData(topoData);
+  topoLayer.addTo(map);
+}
+
+
 
 // Another options object
-var tileOpts = {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 20,
-  ext: 'png'
-};
-var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', tileOpts).addTo(map);
+//var tileOpts = {
+//  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//  subdomains: 'abcd',
+//  minZoom: 0,
+//  ext: 'png'
+//};
+//var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', tileOpts).addTo(map);
 
 // https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json
-var raw = $.ajax('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json')
-.done(function(json){ console.log(json); })
-
+//var raw = $.ajax('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json').done(function(json){ console.log(json); })
+//var data = JSON.aggregate
 
 // grab geojson data
 //var getData = $.ajax('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json');
