06a63499962a49caf7bc2e111056d072c2cfb1bc
JoyceLiu <57802310+joyceyuqiliu@users.noreply.github.com>
2020-04-03 10:30:11 -0400

    updates

--- joyce/js/mtmain.js
+++ joyce/js/mtmain.js
@@ -1,34 +1,108 @@
-var data;  // for holding data
-var stringFilter = "";
-var selectValue = 'All';
-/* =======
-view topojson map
-==== */
-var map = L.map('worldmap').setView([44,-31], 3);
-var topolayer =
+//basemap
+let map = L.map('worldmap');
+      map.setView([0, 0], 2);
+      let positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+      });
 
-data = $.getJSON('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json')
-.done(addTopoData);
+      positron.addTo(map);
 
-function addTopoData(topoData) {
-  topoLayer.addData(topoData);
-  topoLayer.addTo(map);
-}
+//extend Leaflet to create a GeoJSON layer from a TopoJSON file
+      L.TopoJSON = L.GeoJSON.extend({
+        addData: function (data) {
+          var geojson, key;
+          if (data.type === "Topology") {
+            for (key in data.objects) {
+              if (data.objects.hasOwnProperty(key)) {
+                geojson = topojson.feature(data, data.objects[key]);
+                L.GeoJSON.prototype.addData.call(this, geojson);
+              }
+            }
+            return this;
+          }
+          L.GeoJSON.prototype.addData.call(this, data);
+          return this;
+        }
+      });
+      L.topoJson = function (data, options) {
+        return new L.TopoJSON(data, options);
+      };
+//create an empty geojson layer
+      var geojson = L.topoJson(null, {
+        style: function(feature){
+          return {
+            color: "#000",
+            opacity: 1,
+            weight: 1,
+            fillColor: '#35495d',
+            fillOpacity: 0.8
+          }
+        },
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup('<p>'+feature.properties.name+'</p>')
+        }
+      }).addTo(map);
+
+//  var dataset= $.ajax('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json').done(console.log)
+//  var featureGroup;
+
+//  var myStyle = function(feature){
+//    if (dataset.properties.from_name == "United States of America"){
+//      return{color:"red"}}
+//      else{return {color:"green"}}
+//    }
+
+//  var myFilter = function(feature){
+//    if (Math.random()>0.5){
+//      return false
+//    }else {
+//      return true
+//    }
+//  }
+
+//  var eachFeatureFunction = function(layer){
+//    layer.on('click', function(event){
+//      console.log(layer.feature);
+//    showResults();
+//    })
+//  }
+//var featureGroup;
+//var dataset1 = myFilter
+
+//define a function to get and parse geojson from URL
+     async function getGeoData(url) {
+     let response = await fetch(url);
+     let data = await response.json();
+       console.log(data);
+       return data;
+       var paredData = JSON.parse(data);
+       return parsedData}
+
+
+//fetch the topojson and add it to geojson layer
+     getGeoData('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json')
+     .then(parsedData=>geojson.addData(parsedData))
+//     done(function(data) {
+//       var parsedData = JSON.parse(data);
+//       featureGroup = L.geoJson(parsedData, {
+//         style: myStyle,
+//         filter: myFilter
+//       }).addTo(map);
 
 
 
-// Another options object
-//var tileOpts = {
-//  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-//  subdomains: 'abcd',
-//  minZoom: 0,
-//  ext: 'png'
-//};
-//var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', tileOpts).addTo(map);
+//   then(data=>geojson.addData(data))
 
-// https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json
-//var raw = $.ajax('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json').done(function(json){ console.log(json); })
-//var data = JSON.aggregate
 
-// grab geojson data
-//var getData = $.ajax('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json');
+//  $(document).ready(function() {
+//    getGeoData(dataset).done(function(data) {
+//      var parsedData = JSON.parse(data);
+//      featureGroup = L.geoJson(parsedData, {
+//        style: myStyle,
+//        filter: myFilter
+//      }).addTo(map);
+
+//    featureGroup.eachLayer(eachFeatureFunction);
+//              });
