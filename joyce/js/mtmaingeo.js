ac6cc71be83198d711b4f92dd46a20850ef3dd86
JoyceLiu <57802310+joyceyuqiliu@users.noreply.github.com>
2020-04-10 09:07:43 -0400

    update map

---
+++ joyce/js/mtmaingeo.js
@@ -0,0 +1,97 @@
+/* =====================
+  Global Variables
+===================== */
+var data;  // for holding data
+var stringFilter = "";
+var selectValue = 'All';
+
+/* =====================
+  Map Setup
+===================== */
+// Notice that we've been using an options object since week 1 without realizing it
+var map = L.map('worldmap')
+map.setView([0, 0], 2);
+var positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+  subdomains: 'abcd',
+  maxZoom: 20
+});
+positron.addTo(map);
+
+// Ajax to grab json
+var datalink = "https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/m2017.json"
+
+
+var myStyle = function(feature){
+ if(feature.properties.ISO3 == "USA"|
+    feature.properties.ISO3 == "CHN"|
+    feature.properties.ISO3 == "EGY"|
+    feature.properties.ISO3 == "BWA"|
+    feature.properties.ISO3 == "DEU"|
+    feature.properties.ISO3 == "JPN"|
+    feature.properties.ISO3 == "MEX"|
+    feature.properties.ISO3 == "FRA"|
+    feature.properties.ISO3 == "TUR"|
+    feature.properties.ISO3 == "BRA"|
+    feature.properties.ISO3 == "GBR"|
+    feature.properties.ISO3 == "ITA"|
+    feature.properties.ISO3 == "ESP"|
+    feature.properties.ISO3 == "THA"|
+    feature.properties.ISO3 == "KOR"|
+    feature.properties.ISO3 == "COL"|
+    feature.properties.ISO3 == "IRQ"|
+    feature.properties.ISO3 == "MYS"|
+    feature.properties.ISO3 == "AUS"|
+    feature.properties.ISO3 == "POL"
+){
+   return {
+     color: "	rgb(153, 38, 0)",
+     fillColor:"rgb(153, 38, 0)",
+     zIndexOffset: 1000}}
+   else{return{
+     color:"rgb(128, 128, 128)",
+     fillColor:"rgb(128, 128, 128)"}}
+  }
+
+  //add labels
+  var textLatLng = [33.42, 106.514];
+         var myTextLabel = L.marker(textLatLng, {
+             icon: L.divIcon({
+                 className: 'text-labels',   // Set class for CSS styling
+                 html: 'A Text Label'
+             }),
+             zIndexOffset: 1000     // Make appear above other map features
+         });
+
+
+//var myFilter = function(feature) {
+//    if (Math.random()>0.5 ) {
+//      return false
+//    } else {
+//      return true
+//    }
+//  }
+
+
+//var eachFeatureFunction = function(layer) {
+//  layer.on('click', function (event) {
+    /* =====================
+    The following code will run every time a layer on the map is clicked.
+    Check out layer.feature to see some useful data about the layer that
+    you can use in your application.
+    ===================== */
+//    console.log(layer.feature);
+//    showResults();
+//  });
+//};
+
+$(document).ready(function() {
+  $.ajax(datalink).done(function(data) {
+   var parsedData = JSON.parse(data);
+      featureGroup = L.geoJson(parsedData, {
+        style: myStyle
+//         filter: myFilter
+    }).addTo(map);
+//  featureGroup.eachLayer(eachFeatureFunction);
+  });
+});
---
+++ joyce/js/reference/mtmain.js
@@ -0,0 +1,108 @@
+//basemap
+let map = L.map('worldmap');
+      map.setView([0, 0], 2);
+      let positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+      });
+
+      positron.addTo(map);
+
+//extend Leaflet to create a GeoJSON layer from a TopoJSON file
+      L.TopoJSON = L.GeoJSON.extend({
+        addData: function (data) {
+          var geojson, key;
+          if (data.type === "Topology") {
+            for (key in data.objects) {
+              if (data.objects.hasOwnProperty(key)) {
+                geojson = topojson.feature(data, data.objects[key]);
+                L.GeoJSON.prototype.addData.call(this, geojson);
+              }
+            }
+            return this;
+          }
+          L.GeoJSON.prototype.addData.call(this, data);
+          return this;
+        }
+      });
+      L.topoJson = function (data, options) {
+        return new L.TopoJSON(data, options);
+      };
+//create an empty geojson layer
+      var geojson = L.topoJson(null, {
+        style: function(feature){
+          return {
+            color: "#000000",
+            opacity: 1,
+            weight: 1,
+            fillColor: '#35495d',
+            fillOpacity: 0.8
+          }
+        },
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup('<p>'+feature.properties.name+'</p>')
+        }
+      }).addTo(map);
+
+//  var dataset= $.ajax('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3.json').done(console.log)
+//  var featureGroup;
+
+//  var myStyle = function(feature){
+//    if (dataset.properties.from_name == "United States of America"){
+//      return{color:"red"}}
+//      else{return {color:"green"}}
+//    }
+
+//  var myFilter = function(feature){
+//    if (Math.random()>0.5){
+//      return false
+//    }else {
+//      return true
+//    }
+//  }
+
+//  var eachFeatureFunction = function(layer){
+//    layer.on('click', function(event){
+//      console.log(layer.feature);
+//    showResults();
+//    })
+//  }
+//var featureGroup;
+//var dataset1 = myFilter
+
+//define a function to get and parse geojson from URL
+     async function getGeoData(url) {
+     let response = await fetch(url);
+     let data = await response.json();
+       console.log(data);
+       return data}
+//       var parsedData = JSON.parse(data);
+//       return parsedData}
+
+
+//fetch the topojson and add it to geojson layer
+     getGeoData('https://raw.githubusercontent.com/joyceyuqiliu/midterm/master/joyce/midterm_dataset/Waste_Mexport3fromgeo_topos.json')
+     .then(data=>geojson.addData(data))
+//     done(function(data) {
+//       var parsedData = JSON.parse(data);
+//       featureGroup = L.geoJson(parsedData, {
+//         style: myStyle,
+//         filter: myFilter
+//       }).addTo(map);
+
+
+
+//   then(data=>geojson.addData(data))
+
+
+//  $(document).ready(function() {
+//    getGeoData(dataset).done(function(data) {
+//      var parsedData = JSON.parse(data);
+//      featureGroup = L.geoJson(parsedData, {
+//        style: myStyle,
+//        filter: myFilter
+//      }).addTo(map);
+
+//    featureGroup.eachLayer(eachFeatureFunction);
+//              });
